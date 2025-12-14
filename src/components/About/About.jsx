8b141c565265d2c7cf126ec1c1eb4fd790d64c71
Shivam Chamoli <shivamchamoli03@gmail.com>
2025-12-14 06:26:53 +0530

    with changing resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1DgM2sB6A1Yww5D49O_Hsm2vCCQlURhGV/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1RiaftNBYgpHE_lTPP2lDQki9EI1bgys7/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
